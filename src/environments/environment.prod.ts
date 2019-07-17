e426f199b4f18ca8106858a539a29801c9fe0821
CookieRamen <ark@arkjp.net>
2019-07-18 02:36:38 +0900

    [Angular] Fix failed build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  host: 'https://misskey.io',
-  title: 'Misskey.io',
-  apptitle: 'Misskey.io Lite',
+  hostURL: 'https://misskey.io',
+  hostTitle: 'Misskey.io',
+  appTitle: 'Misskey.io Lite',
   logo: 'https://assets.arkjp.net/misskey/lite/lite_logo.svg'
 };
